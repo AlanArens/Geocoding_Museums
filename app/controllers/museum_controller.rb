f75b0278b211b960d6154f1aded54224a12344cd
Alan Arens <natureexplorer@tutanota.com>
2021-11-04 17:26:27 +0100

    scaffolding the app

---
+++ app/controllers/museum_controller.rb
@@ -0,0 +1,16 @@
+class MuseumController < ApplicationController
+
+
+  def index 
+
+    @museums = Museum.all
+
+    @locations = @museums.geocoded.map do |museum|
+      {
+        lat: museum.latitude,
+        lng: museum.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { flat: flat })
+      }
+    end
+  end 
+end
